e6f2b0d88a5e0a9a86c0b8f2e49cdf3a3272e03e
Priyatharsini Sivalingam <112175724+Priya-Sivalingam@users.noreply.github.com>
2024-06-16 02:19:53 +0530

    kitchen page updates

--- src/components/kitchenStaffComponents/kitchenStaffSearchBar.js
+++ src/components/kitchenStaffComponents/kitchenStaffSearchBar.js
@@ -1,21 +1,30 @@
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/material/styles';
+import './kitchenStaffSearchBar.css'; // Import CSS for additional styles
 
-import './kitchenStaffSearchBar.css'; // Import CSS for search bar styles
-/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-<FontAwesomeIcon icon={faSearch} className="search-icon" />*/
-const KitchenStaffSearchBar = ({setSearchText}) => {
-    
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
+const KitchenStaffSearchBar = ({ setSearchText }) => {
   return (
     <div className="kitchen-search-container">
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
       <input
         type="text"
         className="kitchen-search-input"
         placeholder="Search Order ID"
-        onChange={(e)=>setSearchText(e.target.value)}
-        
+        onChange={(e) => setSearchText(e.target.value)}
       />
-      
     </div>
   );
 };
